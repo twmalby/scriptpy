137bcaf6b743c334fc583be067de946e0369e65d
Alberto Ragagnin <59611894+twmalby@users.noreply.github.com>
2020-02-17 14:37:30 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,10 @@
+This program is designed di scrape the web site from  https://www.ken-saku.jp/company_profile.php?id=2.html 
+to https://www.ken-saku.jp/company_profile.php?id=160000.html
+The website contains company pages called as : 2.html, 3.html, 4.html, ..., 100000.html and so on.
+Using > KensakuDownloader.py
+
+After that is it possible convert the content of the selected HTML pages
+into CSV
+launching > Kensaku2CSV.py
+
+That grabs the Name, Address, TEL, FAX, Web, Mail, Comment into CSV row.
